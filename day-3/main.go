56f0ba32c533e8d61a570c3ddcd75ae98fb76802
Collin Farmer <collinthefarmer@gmail.com>
2024-12-03 16:32:33 -0500

    day 3 pt. 2

--- day-3/main.go
+++ day-3/main.go
@@ -20,7 +20,7 @@ func (m *Mul) Exec() int {
 	return m.A * m.B
 }
 
-func extractValidMulCommands(text string) []Mul {
+func extractMulCommands(text string) []Mul {
 	var commands []Mul
 
 	commandPattern := regexp.MustCompile("mul\\([0-9]+,[0-9]+\\)")
@@ -44,8 +44,30 @@ func extractValidMulCommands(text string) []Mul {
 	return commands
 }
 
+func extractEnabledMulCommands(text string) []Mul {
+	var commands []Mul
+
+	textLen := len(text)
+	enabled := true
+	var start int
+	for i := range text {
+		if enabled && i+7 < textLen && text[i:i+7] == "don't()" {
+			enabledCommands := extractMulCommands(text[start:i])
+			commands = append(commands, enabledCommands...)
+			enabled = false
+		} else if !enabled && i+4 < textLen && text[i:i+4] == "do()" {
+			enabled = true
+			start = i + 4
+		}
+
+	}
+
+	return commands
+}
+
 func main() {
-	commands := extractValidMulCommands(input)
+	print("do()"[0:4])
+	commands := extractMulCommands(input)
 
 	var sum int
 	for _, cmd := range commands {
@@ -53,4 +75,12 @@ func main() {
 	}
 
 	log.Printf("Pt. 1: sum of mul commands: %v", sum)
+
+	var enabledSum int
+	enabledCommands := extractEnabledMulCommands(input)
+	for _, cmd := range enabledCommands {
+		enabledSum += cmd.Exec()
+	}
+
+	log.Printf("Pt. 2: sum of enabled mul commands %v", enabledSum)
 }
