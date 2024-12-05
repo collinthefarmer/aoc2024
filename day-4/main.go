8009d39feb2c4db22a9c24a34c7cdce5a3003286
Collin Farmer <collinthefarmer@gmail.com>
2024-12-05 11:11:02 -0500

    reworking to make things easier

--- day-4/main.go
+++ day-4/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	_ "embed"
-	"fmt"
+	//"fmt"
 	//"math"
 	"strings"
 )
@@ -10,96 +10,140 @@ import (
 //go:embed resc/input.txt
 var input string
 
+type Direction int
+
 const (
-	DIR_N  = iota
-	DIR_NE = iota
-	DIR_E  = iota
-	DIR_SE = iota
-	DIR_S  = iota
-	DIR_SW = iota
-	DIR_W  = iota
-	DIR_NW = iota
+	DIR_N  Direction = iota
+	DIR_NE Direction = iota
+	DIR_E  Direction = iota
+	DIR_SE Direction = iota
+	DIR_S  Direction = iota
+	DIR_SW Direction = iota
+	DIR_W  Direction = iota
+	DIR_NW Direction = iota
 )
 
+type Matrix [][]string
+
+type CoordinatePair struct {
+	X int
+	Y int
+}
+
 type SearchResult struct {
-	Position  int
-	Direction int
+	Position  CoordinatePair
+	Direction Direction
 }
 
-func isValidSliceIndex(textLen int, textWidth int, sliceDir int, sliceStart int, index int) bool {
-	var valid bool
-	switch sliceDir {
-	case DIR_N, DIR_S:
-		valid = index >= 0 && index < textLen
-	case DIR_NE:
-		valid = index < textLen && index >= 0 &&
-			index/textWidth < sliceStart/textWidth &&
-			index%textWidth > sliceStart%textWidth
-	case DIR_E:
-		valid = index < textLen &&
-			index/textWidth == sliceStart/textWidth
-	case DIR_W:
-		valid = index >= 0 &&
-			index/textWidth == sliceStart/textWidth
-	}
-	return valid
+func (m *Matrix) Rows() int {
+	return len(*m)
 }
 
-func DirectionalSlice(text string, textLen int, textWidth int, dir int, start int, size int) string {
-	var step int
-	switch dir {
-	case DIR_N:
-		step = -(textWidth + 1) // +1 because of the newline character
-	case DIR_NE:
-		step = -(textWidth)
-	case DIR_E:
-		step = 1
-	case DIR_W:
-		step = -1
-	case DIR_S:
-		step = textWidth + 1
-	default:
-		return ""
-	}
+func (m *Matrix) Columns() int {
+	return len((*m)[0])
+}
 
-	var slice string
-	for i := range size {
-		index := start + i*step
-		if isValidSliceIndex(textLen, textWidth, dir, start, index) {
-			slice += string(text[index])
-		} else {
-			break
+func (m *Matrix) At(coordinates CoordinatePair) string {
+	return (*m)[coordinates.X][coordinates.Y]
+}
+
+func (m *Matrix) DirectionalSubstr(start CoordinatePair, size int, dir Direction) string {
+	return ""
+}
+
+//
+//func isValidSliceIndex(textLen int, textWidth int, sliceDir int, sliceStart int, index int) bool {
+//	var valid bool
+//	switch sliceDir {
+//	case DIR_N, DIR_S:
+//		valid = index >= 0 && index < textLen
+//	case DIR_NE:
+//		fmt.Printf("start:: index: %v, x: %v, y: %v\n", sliceStart, sliceStart%textWidth, sliceStart/textWidth)
+//		fmt.Printf("end:: index: %v, x: %v, y: %v\n\n", index, index%textWidth, index/textWidth)
+//		valid = index == sliceStart ||
+//			index < textLen && index >= 0 &&
+//				index/textWidth < sliceStart/textWidth &&
+//				index%textWidth > sliceStart%textWidth
+//	case DIR_E:
+//		valid = index < textLen &&
+//			index/textWidth == sliceStart/textWidth
+//	case DIR_W:
+//		valid = index >= 0 &&
+//			index/textWidth == sliceStart/textWidth
+//	}
+//	return valid
+//}
+//
+//func DirectionalSlice(text string, textLen int, textWidth int, dir int, start int, size int) string {
+//	var step int
+//	switch dir {
+//	case DIR_N:
+//		step = -(textWidth + 1) // +1 because of the newline character
+//	case DIR_NE:
+//		step = -(textWidth)
+//	case DIR_E:
+//		step = 1
+//	case DIR_W:
+//		step = -1
+//	case DIR_S:
+//		step = textWidth + 1
+//	default:
+//		return ""
+//	}
+//
+//	var slice string
+//	for i := range size {
+//		index := start + i*step
+//		if isValidSliceIndex(textLen, textWidth, dir, start, index) {
+//			slice += string(text[index])
+//		} else {
+//			break
+//		}
+//	}
+//
+//	return slice
+//}
+//
+//func directionalSearch(text string, textLen int, textWidth int, term string) []SearchResult {
+//	var results []SearchResult
+//	for i, char := range text {
+//		if char != rune(term[0]) {
+//			continue
+//		}
+//
+//		for dir := range DIR_NW {
+//			slice := DirectionalSlice(text, textLen, textWidth, dir, i, len(term))
+//			if slice == term {
+//				results = append(results, SearchResult{
+//					Position:  i,
+//					Direction: dir,
+//				})
+//			}
+//		}
+//	}
+//	return results
+//}
+
+func ToMatrix(text string) Matrix {
+	var matrix Matrix
+
+	rows := strings.Split(text, "\n")
+	for _, row := range rows {
+		if len(row) != 0 {
+			matrix = append(matrix, strings.Split(row, ""))
 		}
 	}
 
-	return slice
-}
-
-func search(text string, textLen int, textWidth int, term string) []SearchResult {
-	var results []SearchResult
-	for i, char := range text {
-		if char != rune(term[0]) {
-			continue
-		}
-
-		for dir := range DIR_NW {
-			slice := DirectionalSlice(text, textLen, textWidth, dir, i, len(term))
-			if slice == term {
-				results = append(results, SearchResult{
-					Position:  i,
-					Direction: dir,
-				})
-			}
-		}
-	}
-	return results
+	return matrix
 }
 
 func main() {
-	searchTerm := "XMAS"
-
-	width := strings.Index(input, "\n")
-	results := search(input, len(input), width, searchTerm)
-
-	fmt.Printf("Pt. 1: instances found: %v\n", len(results))
+	//inputMatrix := ToMatrix(input)
+	//
+	//	searchTerm := "XMAS"
+	//
+	//	width := strings.Index(input, "\n")
+	//	results := directionalSearch(input, len(input), width, searchTerm)
+	//
+	//	fmt.Printf("Pt. 1: instances found: %v\n", len(results))
 }
--- day-4/main_test.go
+++ day-4/main_test.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	"strings"
 	"testing"
 )
 
-func TestDirectionalSlice(t *testing.T) {
-	text := `MMMSXXMASM
+func TestRows(t *testing.T) {
+	input := `MMMSXXMASM
 MSAMXMSMSA
 AMXSXMAAMM
 MSAMASMSMX
@@ -17,78 +16,217 @@ SAXAMASAAA
 MAMMMXMMMM
 MXMXAXMASX
 `
-	textLen := len(text)
-	textWidth := strings.Index(text, "\n")
+	mat := ToMatrix(input)
+
+	got := mat.Rows()
+	want := 10
+
+	if got != want {
+		t.Errorf("got %v rows, wanted %v", got, want)
+	}
+}
+func TestColumns(t *testing.T) {
+	input := `MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX
+`
+	mat := ToMatrix(input)
+
+	got := mat.Columns()
+	want := 10
+
+	if got != want {
+		t.Errorf("got %v columns, wanted %v", got, want)
+	}
+}
+
+func TestAt(t *testing.T) {
+	input := `MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX
+`
+	mat := ToMatrix(input)
+
+	got := mat.At(CoordinatePair{0, 0})
+	want := "M"
+
+	if got != want {
+		t.Errorf("got %v, wanted %v", got, want)
+	}
+}
+
+func TestDirectionalSubstr(t *testing.T) {
+	input := `MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX
+`
+	mat := ToMatrix(input)
+	start := CoordinatePair{X: 5, Y: 5}
+	size := 4
 
 	t.Run("DIR_N", func(t *testing.T) {
-		want := "XMAS"
-		got := DirectionalSlice(text, textLen, textWidth, DIR_N, 108, 4)
-
+		got := mat.DirectionalSubstr(start, size, DIR_N)
+		want := "XMSM"
 		if got != want {
-			t.Errorf("got: %v, want: %v", got, want)
+			t.Errorf("got %v, wanted %v", got, want)
 		}
 	})
-
 	t.Run("DIR_NE", func(t *testing.T) {
-		want := "XMAS"
-		got := DirectionalSlice(text, textLen, textWidth, DIR_NE, 104, 4)
-
+		got := mat.DirectionalSubstr(start, size, DIR_NE)
+		want := "XXSM"
 		if got != want {
-			t.Errorf("got: %v, want: %v", got, want)
+			t.Errorf("got %v, wanted %v", got, want)
 		}
 	})
-
 	t.Run("DIR_E", func(t *testing.T) {
-		want := "XMAS"
-		got := DirectionalSlice(text, textLen, textWidth, DIR_E, 0, 4)
-
+		got := mat.DirectionalSubstr(start, size, DIR_E)
+		want := "XXAM"
 		if got != want {
-			t.Errorf("got: %v, want: %v", got, want)
+			t.Errorf("got %v, wanted %v", got, want)
 		}
 	})
-
 	t.Run("DIR_SE", func(t *testing.T) {
-		want := "XMAS"
-		got := DirectionalSlice(text, textLen, textWidth, DIR_SE, 0, 4)
-
+		got := mat.DirectionalSubstr(start, size, DIR_SE)
+		want := "XSAM"
 		if got != want {
-			t.Errorf("got: %v, want: %v", got, want)
+			t.Errorf("got %v, wanted %v", got, want)
 		}
 	})
-
 	t.Run("DIR_S", func(t *testing.T) {
-		want := "XMAS"
-		got := DirectionalSlice(text, textLen, textWidth, DIR_S, 0, 4)
-
+		got := mat.DirectionalSubstr(start, size, DIR_S)
+		want := "XAAX"
 		if got != want {
-			t.Errorf("got: %v, want: %v", got, want)
+			t.Errorf("got %v, wanted %v", got, want)
 		}
 	})
-
 	t.Run("DIR_SW", func(t *testing.T) {
-		want := "XMAS"
-		got := DirectionalSlice(text, textLen, textWidth, DIR_SW, 0, 4)
-
+		got := mat.DirectionalSubstr(start, size, DIR_SW)
+		want := "XSAM"
 		if got != want {
-			t.Errorf("got: %v, want: %v", got, want)
+			t.Errorf("got %v, wanted %v", got, want)
 		}
 	})
-
 	t.Run("DIR_W", func(t *testing.T) {
-		want := "XMAS"
-		got := DirectionalSlice(text, textLen, textWidth, DIR_W, 0, 4)
-
+		got := mat.DirectionalSubstr(start, size, DIR_W)
+		want := "XMMA"
 		if got != want {
-			t.Errorf("got: %v, want: %v", got, want)
+			t.Errorf("got %v, wanted %v", got, want)
 		}
 	})
-
 	t.Run("DIR_NW", func(t *testing.T) {
-		want := "XMAS"
-		got := DirectionalSlice(text, textLen, textWidth, DIR_NW, 0, 4)
-
+		got := mat.DirectionalSubstr(start, size, DIR_NW)
+		want := "XAMX"
 		if got != want {
-			t.Errorf("got: %v, want: %v", got, want)
+			t.Errorf("got %v, wanted %v", got, want)
 		}
 	})
 }
+
+//func TestDirectionalSlice(t *testing.T) {
+//	text := `MMMSXXMASM
+//MSAMXMSMSA
+//AMXSXMAAMM
+//MSAMASMSMX
+//XMASAMXAMM
+//XXAMMXXAMA
+//SMSMSASXSS
+//SAXAMASAAA
+//MAMMMXMMMM
+//MXMXAXMASX
+//`
+//	textLen := len(text)
+//	textWidth := strings.Index(text, "\n")
+//
+//	t.Run("DIR_N", func(t *testing.T) {
+//		want := "XMAS"
+//		got := DirectionalSlice(text, textLen, textWidth, DIR_N, 108, 4)
+//
+//		if got != want {
+//			t.Errorf("got: %v, want: %v", got, want)
+//		}
+//	})
+//
+//	t.Run("DIR_NE", func(t *testing.T) {
+//		want := "XMAS"
+//		got := DirectionalSlice(text, textLen, textWidth, DIR_NE, 108, 4)
+//
+//		if got != want {
+//			t.Errorf("got: %v, want: %v", got, want)
+//		}
+//	})
+//
+//	//	t.Run("DIR_E", func(t *testing.T) {
+//	//		want := "XMAS"
+//	//		got := DirectionalSlice(text, textLen, textWidth, DIR_E, 0, 4)
+//	//
+//	//		if got != want {
+//	//			t.Errorf("got: %v, want: %v", got, want)
+//	//		}
+//	//	})
+//	//
+//	//	t.Run("DIR_SE", func(t *testing.T) {
+//	//		want := "XMAS"
+//	//		got := DirectionalSlice(text, textLen, textWidth, DIR_SE, 0, 4)
+//	//
+//	//		if got != want {
+//	//			t.Errorf("got: %v, want: %v", got, want)
+//	//		}
+//	//	})
+//	//
+//	//	t.Run("DIR_S", func(t *testing.T) {
+//	//		want := "XMAS"
+//	//		got := DirectionalSlice(text, textLen, textWidth, DIR_S, 0, 4)
+//	//
+//	//		if got != want {
+//	//			t.Errorf("got: %v, want: %v", got, want)
+//	//		}
+//	//	})
+//	//
+//	//	t.Run("DIR_SW", func(t *testing.T) {
+//	//		want := "XMAS"
+//	//		got := DirectionalSlice(text, textLen, textWidth, DIR_SW, 0, 4)
+//	//
+//	//		if got != want {
+//	//			t.Errorf("got: %v, want: %v", got, want)
+//	//		}
+//	//	})
+//	//
+//	//	t.Run("DIR_W", func(t *testing.T) {
+//	//		want := "XMAS"
+//	//		got := DirectionalSlice(text, textLen, textWidth, DIR_W, 0, 4)
+//	//
+//	//		if got != want {
+//	//			t.Errorf("got: %v, want: %v", got, want)
+//	//		}
+//	//	})
+//	//
+//	//	t.Run("DIR_NW", func(t *testing.T) {
+//	//		want := "XMAS"
+//	//		got := DirectionalSlice(text, textLen, textWidth, DIR_NW, 0, 4)
+//	//
+//	//		if got != want {
+//	//			t.Errorf("got: %v, want: %v", got, want)
+//	//		}
+//	//	})
+//}
